21048d9500f9e7623fe8cb3417136c5db8a18b8d
Erik-Paulin <152278350+Erik-Paulin@users.noreply.github.com>
2025-03-03 14:53:53 +0100

    lot of stuff

--- app.rb
+++ app.rb
@@ -1,7 +1,118 @@
+require 'bcrypt'
 class App < Sinatra::Base
+
+    def db
+        return @db if @db
+
+        @db = SQLite3::Database.new("db/todos.sqlite")
+        @db.results_as_hash = true
+
+        return @db
+    end
 
     get '/' do
         erb(:"index")
+    end
+
+    configure do
+        enable :sessions
+        set :session_secret, SecureRandom.hex(64)
+    end
+
+    get '/logerror' do
+        erb(:"logerror")
+    end
+      
+    get '/logout' do
+        p "/logout : Logging out"
+        session.clear
+        redirect '/'
+    end
+    post '/login' do
+        request_username = params[:username]
+        request_plain_password = params[:password]
+    
+        user = db.execute("SELECT * FROM user WHERE username = ?", request_username).first
+    
+        unless user
+            p "Invalid username."
+            status 401
+            redirect '/logerror'
+        end
+        
+        db_id = user["id"].to_i
+        db_password_hashed = user["password"].to_s
+    
+        bcrypt_db_password = BCrypt::Password.new(db_password_hashed)
+
+        if bcrypt_db_password == request_plain_password
+            session[:user_id] = db_id
+            session[:admin] = 
+          redirect '/index'
+        else
+          status 401
+          redirect '/logerror'
+        end
+    
+    end
+
+    get '/admin/:id/edit_med' do |id|
+        if session[:admin]
+            @id = params[:id]
+            @med = db.execute('SELECT * FROM med where id=?', id.to_i).first
+            erb(:"/admin/edit_med")
+        else
+            redirect('/unauthorized')
+        end
+    end
+  
+    get '/admin/new_med' do
+        if session[:admin]
+            erb(:"/admin/new_med")
+        else
+            redirect('/unauthorized')
+        end
+    end
+    
+    get '/admin/:id/edit_ill' do |id|
+        if session[:admin]
+            @id = params[:id]
+            @ill = db.execute('SELECT * FROM ill where id=?', id.to_i).first
+            erb(:"/admin/edit_med")
+        else
+            redirect('/unauthorized')
+        end
+    end
+  
+    get '/admin/new_ill' do
+        if session[:admin]
+            erb(:"/admin/new_ill")
+        else
+            redirect('/unauthorized')
+        end
+    end
+
+    post '/admin/:id/update_med' do |id|
+        if session[:admin]
+            name = params[:med_name]
+            desc = params[:med_desc]
+
+            db.execute("UPDATE med SET name=?, description=? WHERE id=?", [name, desc, id])
+            redirect('/index')
+        else
+            redirect('/unauthorized')
+        end
+    end
+    post '/admin/:id/update_ill' do |id|
+        if session[:admin]
+            name = params[:ill_name]
+            desc = params[:ill_desc]
+
+            db.execute("UPDATE ill SET name=?, description=? WHERE id=?", [name, desc, id])
+            redirect('/index')
+        else
+            redirect('/unauthorized')
+        end
     end
 
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -10,27 +10,25 @@ class Seeder
   end
 
   def self.drop_tables
-    db.execute('DROP TABLE IF EXISTS drugs')
-    db.execute('DROP TABLE IF EXISTS illness')
+    db.execute('DROP TABLE IF EXISTS med')
+    db.execute('DROP TABLE IF EXISTS ill')
     db.execute('DROP TABLE IF EXISTS users')
   end
 
   def self.create_tables
-    db.execute('CREATE TABLE drugs (drugid INTEGER PRIMARY KEY AUTOINCREMENT, illnessid1 I, name TEXT NOT NULL, description TEXT)')
-    db.execute('CREATE TABLE illness (illnessid INTEGER PRIMARY KEY AUTOINCREMENT, drugid1 I, name TEXT NOT NULL, description TEXT)')
+    db.execute('CREATE TABLE med (drugid INTEGER PRIMARY KEY AUTOINCREMENT, illnessid1 I, name TEXT NOT NULL, description TEXT)')
+    db.execute('CREATE TABLE ill (illnessid INTEGER PRIMARY KEY AUTOINCREMENT, drugid1 I, name TEXT NOT NULL, description TEXT)')
     db.execute('CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT, access TEXT)')
   end
 
   def self.populate_tables
     password_hashed_admin = BCrypt::Password.create("admin")
     password_hashed_user = BCrypt::Password.create("user")
-    db.execute('INSERT INTO drugs (name, description, illnessid1) VALUES ("drug 1", "first drug", "illness1")')
-    db.execute('INSERT INTO illness (name, description, drugid1) VALUES ("illness 1", "first illness", "drug1")')
+    db.execute('INSERT INTO med (name, description, illnessid1) VALUES ("drug 1", "first drug", "illness1")')
+    db.execute('INSERT INTO ill (name, description, drugid1) VALUES ("illness 1", "first illness", "drug1")')
     db.execute('INSERT INTO users (username, password, access) VALUES (?, ?, ?)',["admin", password_hashed_admin, "admin"])
     db.execute('INSERT INTO users (username, password, access) VALUES (?, ?, ?)',["user", password_hashed_user, "user"])
   end
-
-
 
   private
   def self.db
