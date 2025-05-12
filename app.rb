024c0b439a66aa8bd2307207d88a5d5302f23791
Erik-Paulin <152278350+Erik-Paulin@users.noreply.github.com>
2025-05-12 16:24:54 +0200

    Arbetar på relation mellan ill och med

--- app.rb
+++ app.rb
@@ -19,13 +19,16 @@ class App < Sinatra::Base
     def db
       return @db if @db
 
-      @db = SQLite3::Database.new("db/drugs.sqlite")
+      db_path = "db/drugs.sqlite"
+      @db = SQLite3::Database.new(db_path)
       @db.results_as_hash = true
 
       return @db
     end
 
-    get '/' do
+    get '/index' do
+      @meds = db.execute('SELECT * FROM med')
+      @ills = db.execute('SELECT * FROM ill')
       erb(:"index")
     end
 
@@ -41,7 +44,7 @@ class App < Sinatra::Base
     get '/logout' do
       p "/logout : Logging out"
       session.clear
-      redirect '/'
+      redirect '/index'
     end
 
     get '/login' do
@@ -68,82 +71,154 @@ class App < Sinatra::Base
       if bcrypt_db_password == request_plain_password
         session[:user_id] = db_id
         session[:is_admin] = user['is_admin'] == 1
-        redirect '/'
+        redirect '/index'
       else
         status 401
         redirect '/logerror'
       end
 
     end
+    get '/unauthorized' do
+      erb(:"unauthorized")
+    end
     get '/user/prescript' do
-        user_protected()
+      user_protected()
 
-        user_med_id = session[:user_id]
-        @meds = db.execute('SELECT med FROM user_med WHERE user_id=?'[user_med_id])
+      user_med_id = session[:user_id]
+      @meds = db.execute('SELECT med FROM user_med WHERE user_id=?'[user_med_id])
 
-        erb(:"/user/prescript")
+      erb(:"/user/prescript")
     end
-    get '/admin/new_med' do
+
+    #MEDS
+    get '/meds/show_med/:id' do |id|
+      @med = db.execute('SELECT * FROM med WHERE medid = ?', [id.to_i]).first
+      if @med
+        @ills = db.execute('SELECT ill.* FROM ill INNER JOIN med_ill ON ill.illid = med_ill.ill_id WHERE med_ill.med_id = ?', [id.to_i])
+        erb(:"/meds/show_med")
+      else
+        halt 404, "Medicine not found"
+      end
+    end
+
+    #New medicine
+    get '/admin/meds/new_med' do
       admin_protected()
-
-      erb(:"/admin/new_med")
+      @ills = db.execute('SELECT * FROM ill')
+      erb(:"/admin/meds/new_med")
     end
-    post '/admin/new_med' do
+
+    # New medicine with illness
+    post '/meds/med' do
+      admin_protected()
+      # Get the parameters from the form
       name = params[:med_name]
       desc = params[:med_desc]
+      ill_ids = params[:ill_ids] || []
 
-      db.execute("INSERT med INTO name=?, description=?", [name, desc])
-      redirect('/')
+      db.execute("INSERT INTO med (name, description) VALUES (?, ?)", [name, desc])
+      med_id = db.last_insert_row_id
+
+      ill_ids.each do |ill_id|
+        db.execute("INSERT INTO med_ill (med_id, ill_id) VALUES (?, ?)", [med_id, ill_id])
+      end
+
+      redirect('/index')
     end
-    get '/admin/:id/edit_med' do |id|
+
+    #Edit medicine
+    get '/admin/meds/:id/edit_med' do |id|
       admin_protected()
 
       @id = params[:id]
       @med = db.execute('SELECT * FROM med WHERE medid=?', [id.to_i]).first
-      erb(:"/admin/edit_med")
+      @ills = db.execute('SELECT * FROM ill')
+      @med_ills = db.execute('SELECT ill.* FROM ill INNER JOIN med_ill ON ill.illid = med_ill.ill_id WHERE med_ill.med_id = ?', [id.to_i])
+      erb(:"/admin/meds/edit_med")
     end
-    post '/admin/:id/edit _med' do |id|
+    post '/admin/meds/:id/edit_med/update' do |id|
       admin_protected()
-
       name = params[:med_name]
       desc = params[:med_desc]
+      ill_ids = params[:ill_ids] || []
 
       db.execute("UPDATE med SET name=?, description=? WHERE medid=?", [name, desc, id])
-      redirect('/')
+
+      db.execute("DELETE FROM med_ill WHERE med_id = ?", [id])
+
+      ill_ids.each do |ill_id|
+        db.execute("INSERT INTO med_ill (med_id, ill_id) VALUES (?, ?)", [id, ill_id])
+      end
+      redirect('/index')
     end
-    get '/admin/new_ill' do
+
+
+    post '/admin/meds/remove_med/:id/delete' do | id |
       admin_protected()
-      erb(:"/admin/new_ill")
+      db.execute('DELETE FROM med WHERE medid=?', params['id'])
+      redirect('/index')
     end
-    post '/admin/new_ill' do
+
+    # ILLS
+    get'/ills/show_ill/:id' do |id|
+      @ill = db.execute('SELECT * FROM ill WHERE illid=?', id.to_i).first
+      @meds = db.execute('SELECT med.* FROM med INNER JOIN med_ill ON med.medid = med_ill.med_id WHERE med_ill.ill_id = ?', [id.to_i])
+      erb(:"/ills/show_ill")
+    end
+
+    # New illness
+    get '/admin/ills/new_ill' do
+      admin_protected()
+      @meds = db.execute('SELECT * FROM med')
+      erb(:"/admin/ills/new_ill")
+    end
+    post '/ills/ill' do
+      admin_protected()
       name = params[:ill_name]
       desc = params[:ill_desc]
+      med_ids = params[:med_ids] || []
 
-      db.execute("INSERT INTO ill name=?, description=?", [name, desc])
-      redirect('/')
+      db.execute("INSERT INTO ill (name, description) VALUES (?, ?)", [name, desc])
+      ill_id = db.last_insert_row_id
+
+      med_ids.each do |med_id|
+        db.execute("INSERT INTO med_ill (med_id, ill_id) VALUES (?, ?)", [med_id, ill_id])
+      end
+      redirect('/index')
     end
-    get '/admin/:id/edit_ill' do |id|
+
+    # Edit illness
+    get '/admin/ills/:id/edit_ill' do |id|
       admin_protected()
 
       @id = params[:id]
       @ill = db.execute('SELECT * FROM ill WHERE illid=?', id.to_i).first
-      erb(:"/admin/edit_med")
-    end
-    post '/admin/:id/edit_ill' do |id|
-      admin_protected()
+      @meds = db.execute('SELECT * FROM med')
 
+      @ill_meds = db.execute('SELECT med_id FROM med_ill WHERE ill_id = ?', [id.to_i]).map { |row| row[:med_id] }
+
+      erb(:"/admin/ills/edit_ill")
+    end
+    post '/admin/ills/:id/edit_ill/update' do |id|
+      admin_protected()
       name = params[:ill_name]
       desc = params[:ill_desc]
+      med_ids = params[:med_ids] || []
 
       db.execute("UPDATE ill SET name=?, description=? WHERE illid=?", [name, desc, id])
-      redirect('/')
+
+      db.execute("DELETE FROM med_ill WHERE ill_id = ?", [id])
+
+      med_ids.each do |med_id|
+        db.execute("INSERT INTO med_ill (med_id, ill_id) VALUES (?, ?)", [med_id, id])
+      end
+      redirect('/index')
     end
 
-    post '/ill' do
-      name = params[:ill_name]
-      desc = params[:ill_desc]
-
-      db.execute("INSERT INTO ill (name, description) VALUES (?, ?)", [name, desc])
-      redirect('/')
+    # Remove illness
+    post '/admin/ills/remove_ill/:id/delete' do | id |
+      admin_protected()
+      db.execute('DELETE FROM ill WHERE illid=?', params['id'])
+      redirect('/index')
     end
 end
--- db/seeder.rb
+++ db/seeder.rb
@@ -14,23 +14,32 @@ class Seeder
     db.execute('DROP TABLE IF EXISTS ill')
     db.execute('DROP TABLE IF EXISTS users')
     db.execute('DROP TABLE IF EXISTS user_med')
-
+    db.execute('DROP TABLE IF EXISTS med_ill')
   end
 
   def self.create_tables
-    db.execute('CREATE TABLE med (medid INTEGER PRIMARY KEY AUTOINCREMENT, illnessid1 I, name TEXT NOT NULL, description TEXT)')
-    db.execute('CREATE TABLE ill (illid INTEGER PRIMARY KEY AUTOINCREMENT, drugid1 I, name TEXT NOT NULL, description TEXT)')
+    db.execute('CREATE TABLE med (medid INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, description TEXT)')
+    db.execute('CREATE TABLE ill (illid INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, description TEXT)')
     db.execute('CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, username TEXT NOT NULL, password TEXT NOT NULL, is_admin INTEGER)')
-
+    db.execute('CREATE TABLE med_ill (med_id INTEGER, ill_id INTEGER, FOREIGN KEY (med_id) REFERENCES med(medid), FOREIGN KEY (ill_id) REFERENCES ill(illid), PRIMARY KEY (med_id, ill_id))')
   end
 
   def self.populate_tables
     password_hashed_admin = BCrypt::Password.create("admin")
     password_hashed_user = BCrypt::Password.create("user")
-    db.execute('INSERT INTO med (name, description, illnessid1) VALUES ("drug 1", "first drug", "illness1")')
-    db.execute('INSERT INTO ill (name, description, drugid1) VALUES ("illness 1", "first illness", "drug1")')
+
+    db.execute('INSERT INTO med (name, description) VALUES ("Drug 1", "First drug")')
+    db.execute('INSERT INTO med (name, description) VALUES ("Drug 2", "Second drug")')
+
+    db.execute('INSERT INTO ill (name, description) VALUES ("Illness 1", "First illness")')
+    db.execute('INSERT INTO ill (name, description) VALUES ("Illness 2", "Second illness")')
+
     db.execute('INSERT INTO users (username, password, is_admin) VALUES (?, ?, ?)',["admin", password_hashed_admin, 1])
     db.execute('INSERT INTO users (username, password, is_admin) VALUES (?, ?, ?)',["user", password_hashed_user, 0])
+
+    db.execute('INSERT INTO med_ill (med_id, ill_id) VALUES (1, 1)')
+    db.execute('INSERT INTO med_ill (med_id, ill_id) VALUES (2, 2)')
+    db.execute('INSERT INTO med_ill (med_id, ill_id) VALUES (1, 2)')
   end
 
   private
